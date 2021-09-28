b473f0d694faecd68acb8cf99c731a2c259ab5b1
Anatolii Neveselyi <anatolii.neveselyi@gmail.com>
2021-09-28 11:47:42 +0300

    add file

---
+++ Jenkinsfile
@@ -0,0 +1,110 @@
+pipeline {
+    agent any
+
+    stages {
+    
+        stage('Terraform Init'){
+            
+            steps {
+             ansiColor('xterm') {
+                  withCredentials([azureServicePrincipal(
+                    credentialsId: 'jenkins',
+                    subscriptionIdVariable: 'ARM_SUBSCRIPTION_ID',
+                    clientIdVariable: 'ARM_CLIENT_ID',
+                    clientSecretVariable: 'ARM_CLIENT_SECRET',
+                    tenantIdVariable: 'ARM_TENANT_ID')]) {
+                         sh """
+                        echo " Terraform init"
+                        terraform init 
+                        """
+                    }
+                }
+                    
+            }
+        }
+
+        
+       stage('Terraform Plan'){
+            
+            steps {
+             ansiColor('xterm') {
+                  withCredentials([azureServicePrincipal(
+                    credentialsId: 'jenkins',
+                    subscriptionIdVariable: 'ARM_SUBSCRIPTION_ID',
+                    clientIdVariable: 'ARM_CLIENT_ID',
+                    clientSecretVariable: 'ARM_CLIENT_SECRET',
+                    tenantIdVariable: 'ARM_TENANT_ID')]) {
+                         sh """
+                        echo "Creating Terraform plan"
+                        terraform plan
+                        """
+                    }
+                }
+                    
+            }
+        }
+
+        stage('Waiting for Approval'){
+            steps {
+                timeout(time: 10, unit: 'MINUTES') {
+                    input (message: "Do you agree to create the infrastructure?")
+                }
+            }
+        
+        }
+
+      stage('Terraform Apply'){
+            
+            steps {
+             ansiColor('xterm') {
+                  withCredentials([azureServicePrincipal(
+                    credentialsId: 'jenkins',
+                    subscriptionIdVariable: 'ARM_SUBSCRIPTION_ID',
+                    clientIdVariable: 'ARM_CLIENT_ID',
+                    clientSecretVariable: 'ARM_CLIENT_SECRET',
+                    tenantIdVariable: 'ARM_TENANT_ID')]) {
+                         sh """
+                        echo "Terraform Apply "
+                        terraform apply -auto-approve 
+                        """
+                       script {
+                        env.VM_IP = sh(script:'terraform output vm_ip', returnStdout: true).trim()
+                       }
+                      sh 'echo "[dev]" > /home/anv/inventory'
+                      sh 'echo "dev_host1 ansible_ssh_port=50001 ansible_ssh_host=$VM_IP" >> /home/anv/inventory'
+                      sh 'echo "dev_host2 ansible_ssh_port=50002 ansible_ssh_host=$VM_IP" >> /home/anv/inventory'
+                    }
+                }
+                    
+            }
+        }
+        stage('Waiting for Destroy'){
+            steps {
+                timeout(time: 12, unit: 'HOURS') {
+                    input (message: "Are you sure destroying the infrastructure?")
+                }
+            }
+        
+        }
+
+        stage('Terraform Destroy'){
+            
+            steps {
+             ansiColor('xterm') {
+                  withCredentials([azureServicePrincipal(
+                    credentialsId: 'jenkins',
+                    subscriptionIdVariable: 'ARM_SUBSCRIPTION_ID',
+                    clientIdVariable: 'ARM_CLIENT_ID',
+                    clientSecretVariable: 'ARM_CLIENT_SECRET',
+                    tenantIdVariable: 'ARM_TENANT_ID')]) {
+                         sh """
+                        echo "Terraform Destroy "
+                        terraform destroy -auto-approve 
+                        """
+                    }
+                }
+                    
+            }
+        }
+    }
+}
---
+++ main.tf
@@ -0,0 +1,180 @@
+terraform {
+
+  required_version = "=1.0.7"
+  
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=2.77.0"
+    }
+  }
+
+    backend "azurerm" {
+        resource_group_name  = "system_resource_group"
+        storage_account_name = "storageforsystemdata"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "Dev_stage"
+  location = "East US"
+
+  tags = {
+    environment = "Dev_stage"
+  }
+}
+
+
+
+resource "azurerm_availability_set" "avset" {
+  name                         = "${var.dns_name}avset"
+  location                     = "${azurerm_resource_group.rg.location}"
+  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+  managed                      = true
+}
+
+resource "azurerm_public_ip" "lbpip" {
+  name                         = "${var.lb_ip_dns_name}-ip"
+  location                     = "${azurerm_resource_group.rg.location}"
+  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  allocation_method            = "Static"
+  domain_name_label            = "${var.lb_ip_dns_name}"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.virtual_network_name}"
+  location            = "${azurerm_resource_group.rg.location}"
+  address_space       = ["10.0.0.0/16"]
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.rg_prefix}subnet"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_lb" "lb" {
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  name                = "${var.rg_prefix}lb"
+  location            = "${azurerm_resource_group.rg.location}"
+
+  frontend_ip_configuration {
+    name                 = "LoadBalancerFrontEnd"
+    public_ip_address_id = "${azurerm_public_ip.lbpip.id}"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+  loadbalancer_id     = "${azurerm_lb.lb.id}"
+  name                = "BackendPool1"
+}
+
+resource "azurerm_lb_nat_rule" "tcp" {
+  resource_group_name            = "${azurerm_resource_group.rg.name}"
+  loadbalancer_id                = "${azurerm_lb.lb.id}"
+  name                           = "SSH-VM-${count.index}"
+  protocol                       = "tcp"
+  frontend_port                  = "5000${count.index + 1}"
+  backend_port                   = 22
+  frontend_ip_configuration_name = "LoadBalancerFrontEnd"
+  count                          = 2
+}
+
+resource "azurerm_lb_rule" "lb_rule" {
+  resource_group_name            = "${azurerm_resource_group.rg.name}"
+  loadbalancer_id                = "${azurerm_lb.lb.id}"
+  name                           = "LBRule"
+  protocol                       = "tcp"
+  frontend_port                  = 80
+  backend_port                   = 8080
+  frontend_ip_configuration_name = "LoadBalancerFrontEnd"
+  enable_floating_ip             = false
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.backend_pool.id}"
+  idle_timeout_in_minutes        = 5
+  probe_id                       = "${azurerm_lb_probe.lb_probe.id}"
+  depends_on                     = [azurerm_lb_probe.lb_probe]
+}
+
+resource "azurerm_lb_probe" "lb_probe" {
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  loadbalancer_id     = "${azurerm_lb.lb.id}"
+  name                = "tcpProbe"
+  protocol            = "tcp"
+  port                = 8080
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic${count.index}"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  count               = 2
+
+  ip_configuration {
+    name                                    = "ipconfig${count.index}"
+    subnet_id                               = "${azurerm_subnet.subnet.id}"
+    private_ip_address_allocation           = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "nic_to_backend" {
+  network_interface_id    ="${azurerm_network_interface.nic[count.index].id}"
+  ip_configuration_name   = "ipconfig${count.index}"
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.backend_pool.id}"
+  count                 = 2
+}
+resource "azurerm_network_interface_nat_rule_association" "nic_to_nat" {
+  network_interface_id  = "${azurerm_network_interface.nic[count.index].id}"
+  ip_configuration_name = "ipconfig${count.index}"
+  nat_rule_id           = "${azurerm_lb_nat_rule.tcp[count.index].id}"
+  count                 = 2
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "vm${count.index}"
+  location              = "${azurerm_resource_group.rg.location}"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  admin_username        = "anv"
+  availability_set_id   = "${azurerm_availability_set.avset.id}"
+  network_interface_ids = ["${element(azurerm_network_interface.nic.*.id, count.index)}"]
+  count                 = 2
+  size                   = "Standard_F2"
+
+   os_disk {
+    name                ="osdisk${count.index}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+   source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "20.04.202004230"
+  }
+  
+   admin_ssh_key {
+    username   = "anv"
+    public_key = file("/var/lib/jenkins/.ssh/id_rsa.pub")
+  }
+
+  
+
+  tags = {
+    environment = "Dev_stage"
+  }
+}
